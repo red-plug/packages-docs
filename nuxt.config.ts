f8ace43b8b687d1daa73998b6fc3c425a9992a23
andreslqr <me@andreslopez.com.mx>
2024-08-07 11:53:23 -0600

    removed image optimizing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-lodash', "@nuxt/image"],
+    modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-lodash'],
     routeRules: {
         '/': { prerender: true }
     },
